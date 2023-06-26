372caac302515ad3a5e87aa48fe81d4a6a43ad2f
laila22haz <hazmirlaila316@gmail.com>
2023-06-26 01:11:01 +0100

    22th update

--- monty.h
+++ monty.h
@@ -65,6 +65,7 @@ void _mod(stack_t **stack, unsigned int line_number);
 void _pchar(stack_t **stack, unsigned int line_number);
 void _pstr(stack_t **stack, unsigned int line_number);
 void _rotl(stack_t **stack, unsigned int line_number);
+void _rotr(stack_t **stack, unsigned int line_number)
 
 /** error functions **/
 
